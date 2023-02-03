a59db90384f7f10dab76c29ec4a1db7a0a26b194
Chayce Knaub <chayceknaub@Chayces-MacBook-Pro.local>
2023-02-03 14:55:26 -0500

    ts and react18 adjustments

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const rootEl = (document.getElementById('root'));
+// if (!rootEl) throw new Error('Failed to find the root element');
+// const root = createRoot(rootEl);
+// root.render(<h1>Hello, world!</h1>);
+
+// 1) Import ReactDOM library
+import ReactDOM from "react-dom/client";
+ 
+// 2) Get a reference to the div with ID root
+const el = document.getElementById("root");
+ 
+// 3) Tell React to take control of that element
+// NULL CHECK
+const root = ReactDOM.createRoot(el!);
+ 
+// 4) Create a component
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there WITH NULL CHECK!</h1>
+    </div>
+  );
+};
+ 
+// 5) Show the component on the screen
+root.render(<App />);
